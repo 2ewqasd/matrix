2555142e840cdb40c13cea67a8c39ec7dcd88a8d
Влад Радченко <vr@MacBook-Pro-Vlad-2.local>
2021-05-06 17:56:42 +0300

    Replacing elements above and below the main and secondary diagonal

--- matrix.py
+++ matrix.py
@@ -20,10 +20,26 @@ def refill(matrix):
     for i in range(len(matrix)):
         matrix[i][-i - 1] = 2
 
+def upper_null(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if (j >= i):
+                matrix[i][j] = 3
+
+def low_null(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if (j <= i):
+                matrix[i][j] = 4
+
 def main():
     matrix = init_matrix()
     fill(matrix)
     refill(matrix)
+    pprint(matrix)
+    upper_null(matrix)
+    pprint(matrix)
+    low_null(matrix)
     pprint(matrix)
 
 if __name__ == '__main__':
